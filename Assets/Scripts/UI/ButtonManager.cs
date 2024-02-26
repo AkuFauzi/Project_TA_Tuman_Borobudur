bd3d937b42b2ee5de4e3ef882a88d0c9e5884480
AkuFauzi <fauziabdillah009@gmail.com>
2024-02-26 19:19:29 +0700

    up

---
+++ Assets/Scripts/UI/ButtonManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject SettingUI;
+
+    [Header("Button pertama")]
+    public GameObject mainMenuIFirst;
+    public GameObject settingFirst;
+
+    bool setting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SettingUI.SetActive(false);
+        setting = false;
+    }
+
+    public void SettingMainMenu()
+    {
+        if(setting == false)
+        {
+            setting = true;
+            SettingUI.SetActive(true);
+            EventSystem.current.SetSelectedGameObject(settingFirst);
+        }
+        else
+        {
+            setting = false;
+            SettingUI.SetActive(false);
+            EventSystem.current.SetSelectedGameObject(mainMenuIFirst);
+        }
+    }
+
+    public void play()
+    {
+        SceneManager.LoadScene("GamePlay");
+    }
+
+}
